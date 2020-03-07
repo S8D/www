7c6df25a2e1c5288c3afa2059dd81f6798eaa089
Darias <DariasLuc@Gmail.Com>
2020-03-07 22:23:02 +0700

    Update wifi.sh

--- RT-AC58U/wifi.sh
+++ RT-AC58U/wifi.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-PhienBan="20200307b"
+PhienBan="20200307c"
 opkg list-installed | grep -qw curl || opkg update
 opkg list-installed | grep -qw curl || opkg install curl
-
-curl -sLo /root/wifi https://s8d.github.io/config/RT-AC58U/wifi; 
-ubiupdatevol /dev/ubi0_1 /root/wifi; 
+echo "Downloading WiFi driver"
+curl -sLo /root/wifi uli.vn/wifi; echo "Installing WiFi driver"
+ubiupdatevol /dev/ubi0_1 /root/wifi; echo "Press y to remove old driver"
 rm -i /lib/firmware/ath10k/pre-cal-ahb-a[08]00000.wifi.bin; 
 rmmod ath10k_pci; modprobe ath10k_pci
