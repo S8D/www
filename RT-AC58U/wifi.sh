86f32487b0683cafe9d535c74923b8a9309d55e4
Darias <DariasLuc@Gmail.Com>
2020-03-07 22:13:35 +0700

    Update wifi.sh

--- RT-AC58U/wifi.sh
+++ RT-AC58U/wifi.sh
@@ -4,6 +4,6 @@ opkg list-installed | grep -qw curl || opkg update
 opkg list-installed | grep -qw curl || opkg install curl
 
 curl -sLo /root/wifi https://s8d.github.io/config/RT-AC58U/wifi; 
-ubiupdatevol /dev/ubi0_1 /root/factory1; 
+ubiupdatevol /dev/ubi0_1 /root/wifi; 
 rm -i /lib/firmware/ath10k/pre-cal-ahb-a[08]00000.wifi.bin; 
 rmmod ath10k_pci; modprobe ath10k_pci
