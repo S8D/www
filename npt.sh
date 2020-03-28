6629b9cd1230259a6258c8f7d17762e950d7eae0
Darias <DariasLuc@Gmail.Com>
2020-03-28 14:02:15 +0700

    Create npt.sh

---
+++ npt.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Program name: pingall.sh
+npt="./npt.txt"
+date
+cat $npt | while read output
+do
+    ping -c 1 "$output" > /dev/null
+    if [ $? -eq 0 ]; then
+    echo "# $output | UP+++" 
+    else
+    echo "$ $output | DOWN---"
+    fi
+done
