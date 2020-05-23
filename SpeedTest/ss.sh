822de4a8d010e2677ffc2b6717b65fbe35124950
Darias <DariasLuc@Gmail.Com>
2020-05-23 20:46:05 +0700

    Update ss.sh

--- SpeedTest/ss.sh
+++ SpeedTest/ss.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
-PhienBan="20200516a"
+PhienBan="20200523a"
 UpLink="https://bom.to/sss"
 pem="/etc/ssl/cert.pem"; mkdir -p /etc/ssl
 uPem="https://bom.to/pem"
 u64="https://bom.to/sp64"
 uArm="https://bom.to/sparm"
 uAR="https://bom.to/sp64a"
-SG="13623"
+umip="https://bom.to/spp"
+SG="4235"
 HK="16176"
 VN="6106"
 GetTime=$(date +"%F %a %T"); Time="$GetTime -"; DauCau="#"
 
 if [ ! -f "$pem" ]; then echo "Đang tải chứng chỉ..."; curl -sLo $pem $uPem; fi
 
-OS=`uname -m`; x64="x86_64"; arm="armv7l"; Android="aarch64"
+OS=`uname -m`; x64="x86_64"; arm="armv7l"; Android="aarch64"; mip="mips"
 
 if [ $OS == $x64 ] || [ $OS == $arm ]; then TM="/sd"; mkdir -p $TM; fi
 if [ $OS == $Android ]; then TM="/sdcard"; mkdir -p $TM; fi
@@ -21,6 +22,7 @@ SP="$TM/sp"; mkdir -p $SP; upTam="$SP/tam"
 if [ $OS == $x64 ]; then upem=$u64; sp="/usr/sbin/sp"; fi
 if [ $OS == $arm ]; then upem=$uArm; if [ -d /www/cgi-bin ]; then sp="/usr/sbin/sp"; else sp="/opt/sbin/sp"; fi; fi
 if [ $OS == $Android ]; then upem=$uAR; sp="/system/xbin/sp"; fi
+if [ $OS == $mip ]; then upem=$umip; sp="/system/xbin/sp"; fi
 
 if [ ! -f "$sp" ]; then echo "Đang tải SpeedTest..."; curl -sLo $sp $upem; chmod +x $sp; fi
 if [ $OS != $x64 ] && [ $OS != $arm ] && [ $OS != $Android ]; then echo "Chưa hỗ trợ hệ thống bạn đang dùng"; exit 1; fi
@@ -29,12 +31,12 @@ Giup ()
 {
 	echo ""
 	echo "Cú pháp:"
-	printf '\t'; echo "$(basename "$0") [ -h | -a | -s | -v ]"
+	printf '\t'; echo "$(basename "$0") [ -h | -a | -s | -k | -v | -t [máy chủ] ]"
 	echo ""
 	echo "Chức năng:"
 	printf '\t'; echo -n "[ -h ]"; printf '\t'; echo "Hiện hướng dẫn sử dụng"
 	printf '\t'; echo -n "[ -a ]"; printf '\t'; echo "Kiểm tra tốc độ mạng tới tất cả máy chủ"
-	printf '\t'; echo -n "[ -s ]"; printf '\t'; echo "Kiểm tra tốc độ mạng tới máy chủ Singapore"
+	printf '\t'; echo -n "[ -s ]"; printf '\t'; echo "Kiểm tra tốc độ mạng tới máy chủ Singapore [4235: StarHub | 7556: PT FirstMedia | 7311: M1 Limited | 20637: OVH Cloud | 5168: PT Indosat Tbk | 18791: FPT Telecom]"
 	printf '\t'; echo -n "[ -k ]"; printf '\t'; echo "Kiểm tra tốc độ mạng tới máy chủ Hong Kong"
 	printf '\t'; echo -n "[ -v ]"; printf '\t'; echo "Kiểm tra tốc độ mạng tới máy chủ Việt Nam"
 	printf '\t'; echo -n "[ -t ]"; printf '\t'; echo "Kiểm tra tốc độ mạng tới máy chủ tùy chọn"
@@ -44,11 +46,11 @@ Giup ()
 while getopts "h?askvt" opt; do echo ""
 	case ${opt} in
 		h|\? ) Giup ;;
-		a    ) echo "Kiểm tra tốc độ mạng tới tất cả máy chủ"; sp -B -s $SG; sp -B -s $HK; sp -B -s $VN ;;
-		s    ) echo "Kiểm tra tốc độ mạng tới máy chủ Singapore"; sp -B -s $SG ;;
-		k    ) echo "Kiểm tra tốc độ mạng tới máy chủ Hong Kong"; sp -B -s $HK ;;
-		v    ) echo "Kiểm tra tốc độ mạng tới máy chủ Việt Nam"; sp -B -s $VN ;;
-		t    ) echo "Kiểm tra tốc độ mạng tới máy chủ tùy chọn"; echo -en "Nhập ID máy chủ: "; read -r t; sp -B -s $t ;;
+		a    ) echo "Kiểm tra tốc độ mạng tới tất cả máy chủ"; if [ $OS == $mip ]; then sp --bytes --secure --server $SG; sp --bytes --secure --server $HK; sp --bytes --secure --server $VN; else sp -B -s $SG; sp -B -s $HK; sp -B -s $VN; fi ;;
+		s    ) echo "Kiểm tra tốc độ mạng tới máy chủ Singapore"; if [ $OS == $mip ]; then sp --bytes --secure --server $SG; else sp -B -s $SG; fi ;;
+		k    ) echo "Kiểm tra tốc độ mạng tới máy chủ Hong Kong"; if [ $OS == $mip ]; then sp --bytes --secure --server $HK; else sp -B -s $HK; fi ;;
+		v    ) echo "Kiểm tra tốc độ mạng tới máy chủ Việt Nam"; if [ $OS == $mip ]; then sp --bytes --secure --server $VN; else sp -B -s $VN; fi ;;
+		t    ) echo "Kiểm tra tốc độ mạng tới máy chủ tùy chọn"; echo -en "Nhập ID máy chủ: "; read -r t; if [ $OS == $mip ]; then sp --bytes --secure --server $t; else sp -B -s $t; fi ;;
 		
 	\? ) exit 2 ;;
 	esac
