96740a5bc22e221b8f0e796fc7f86d73f2ebd688
Darias <DariasLuc@Gmail.Com>
2020-10-11 23:48:39 +0700

    Update ub.sh

--- UnBound/ub.sh
+++ UnBound/ub.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-PhienBan="201011c"
+PhienBan="201011d"
 GetTime=$(date +"%F %a %T"); Time="$GetTime -"; DauCau="#"
-TM="/sd/"; TMunb="${TM}/unb";
+TM="/sd"; mkdir -p $TM; TMunb="${TM}/unb"; mkdir -p $TMunb
 Log="${TMunb}/NhatKy.log"; if [ ! -f "$Log" ]; then echo '' > $Log; fi
 
 up1="https://bom.to/_ub"
@@ -20,20 +20,20 @@ DonDep () {
 }
 
 clear; echo -e "\n$ Đang kiểm tra máy chủ cập nhật..."
-CheckNet_1 () { ping -q -c 1 -W 1 bom.to >/dev/null; }; 
-CheckNet_2 () { ping -q -c 1 -W 1 tiny.cc >/dev/null; }; 
-CheckNet_3 () { ping -q -c 1 -W 1 gg.gg >/dev/null; }; 
-CheckNet_4 () { ping -q -c 1 -W 1 github.com >/dev/null; }; DonDep;	
+CheckNet_1 () { ping -q -c 1 -W 1 bom.to >/dev/null; };
+CheckNet_2 () { ping -q -c 1 -W 1 tiny.cc >/dev/null; };
+CheckNet_3 () { ping -q -c 1 -W 1 gg.gg >/dev/null; };
+CheckNet_4 () { ping -q -c 1 -W 1 github.com >/dev/null; }; DonDep;
 if CheckNet_1; then UpLink="${up1}"; DownLink="${dl1}"; net="1"; else
 	if CheckNet_2; then UpLink="${up2}"; DownLink="${dl2}"; net="2"; else
-		if CheckNet_3; then UpLink="${up3}"; DownLink="${dl3}"; net="3"; else	
-			if CheckNet_4; then UpLink="${up4}"; DownLink="${dl4}"; net="4"; else net=0; 
+		if CheckNet_3; then UpLink="${up3}"; DownLink="${dl3}"; net="3"; else
+			if CheckNet_4; then UpLink="${up4}"; DownLink="${dl4}"; net="4"; else net=0;
 			fi
 		fi
 	fi
 fi
 
-echo -e "\n$DauCau Đang kiểm tra phiên bản...";
+echo -e "\n$DauCau Đang kiểm tra phiên bản UnBound...";
 PhienBanOff=$(unbound -V | grep Version | sed 's/Version //')
 PhienBanOn=$(curl -sL https://tiny.cc/ub_ | grep release- | cut -d\" -f4 | grep [0-9]$ | sed 's/.*\-//' | sed -n '1p')
 DownURL="https://github.com/NLnetLabs/unbound/archive/release-${PhienBanOn}.tar.gz"
@@ -56,13 +56,13 @@ KiemUB () {
 		echo "$DauCau UnBound $PhienBanOn là bản mới nhất!"; exit 1; else
 		echo "$DauCau Đang cập nhật UnBound v.$PhienBanOff lên v.$PhienBanOn..."
 		echo "$DauCau Đang tải UnBound..."
-		curl -sLo ${TMunb}/unb.tar.gz ${DownURL};	
-		echo -e "$DauCau Đang giải nén UnBound..."; 
+		curl -sLo ${TMunb}/unb.tar.gz ${DownURL};
+		echo -e "$DauCau Đang giải nén UnBound...";
 		cd $TMunb; tar xzf unb.tar.gz
-		if [ ! -f ${TMunb}/unbound-*/LICENSE ]; then echo -e "\n$DauCau Giải nén thất bại!!! Thoát ra!"; DonDep; exit; 
+		if [ ! -f ${TMunb}/unbound-*/LICENSE ]; then echo -e "\n$DauCau Giải nén thất bại!!! Thoát ra!"; DonDep; exit;
 			else echo "$DauCau Giải thành công!!!"; BuildUB
 		fi
-		
+	
 	fi
 }
 
@@ -74,7 +74,7 @@ KiemSH () {
 				cp $0 ${TMunb}/$PhienBan\_$(basename "$0")
 				curl -sLo $upTam $UpLink; chmod +x $upTam; mv $upTam ${TMunb}/$0
 				echo "$Time $(basename "$0") được cập nhật lên $PhienBanMoi!"  >> $Log
-				echo "$DauCau Khởi chạy $(basename "$0") $PhienBanMoi..."; 
+				echo "$DauCau Khởi chạy $(basename "$0") $PhienBanMoi...";
 				sh ${TMunb}/$(basename "$0")
 		fi
 	fi
