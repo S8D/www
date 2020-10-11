8514df4f7fe15c52e5fc8a94550f490764231399
Darias <DariasLuc@Gmail.Com>
2020-10-12 00:27:00 +0700

    Update ub.sh

--- UnBound/ub.sh
+++ UnBound/ub.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PhienBan="201012d"
+PhienBan="201012e"
 GetTime=$(date +"%F %a %T"); Time="$GetTime -"; DauCau="#"
 TM="/sd"; mkdir -p $TM; TMunb="${TM}/unb"; mkdir -p $TMunb
 Log="${TMunb}/NhatKy.log"; if [ ! -f "$Log" ]; then echo '' > $Log; fi
@@ -33,12 +33,29 @@ if CheckNet_1; then UpLink="${up1}"; DownLink="${dl1}"; net="1"; else
 	fi
 fi
 
+KiemSH () {
+	if [ $net -ge 1 ]; then echo "$DauCau Đang kiểm tra cập nhật $(basename "$0") $PhienBan...";
+		PhienBanMoi=$(curl -sL "${UpLink}" | grep PhienBan\= | sed 's/.*\=\"//; s/\"$//');
+		if [ $PhienBanMoi == $PhienBan ]; then echo "$DauCau $(basename "$0") $PhienBan là phiên bản mới nhất!";
+			else echo "$DauCau Đang cập nhật $(basename "$0") v.$PhienBan lên v.$PhienBanMoi...";
+				cp $0 ${TMunb}/$PhienBan\_$(basename "$0");
+				curl -sLo $upTam $UpLink; chmod +x $upTam; 
+				#mv $upTam ${TMunb}/$(basename "$0")
+				mv $upTam $0;
+				echo "$Time $(basename "$0") được cập nhật lên $PhienBanMoi!"  >> $Log;
+				echo "$DauCau Khởi chạy $(basename "$0") $PhienBanMoi...";
+				#sh ${TMunb}/$(basename "$0"); 
+				sh $0
+				exit 1; fi
+	fi
+}
+
 BuildUB () {
 	echo -e "\n$DauCau Đang build UnBound ${PhienBanOn}...\n\n"
 	cd ${TMunb}/unbound-*
 	./configure --prefix=/usr --disable-static --enable-dnscrypt --enable-subnet --includedir=${prefix}/include --infodir=${prefix}/share/info --libdir=/usr/lib --localstatedir=/var --mandir=${prefix}/share/man --sysconfdir=/etc --with-libevent --with-pidfile=/run/unbound.pid --with-rootkey-file=/etc/unbound/root.key
 	make && make install && clear
-	echo -e "\n$DauCau Đang kiểm tra phiên bản...";
+	echo -e "\n$DauCau Đang kiểm tra phiên bản UnBound...";
 	if [ $PhienBanOn == $PhienBanOff ]; then echo "$Time UnBound $PhienBanOn là bản mới nhất!" >> $Log;
 		echo "$DauCau UnBound $PhienBanOn là bản mới nhất!"; exit 1; else
 		echo "$DauCau Cập nhật UnBound thất bại!!!"; DonDep; exit 1; fi
@@ -57,19 +74,6 @@ KiemUB () {
 		cd $TMunb; tar xzf unb.tar.gz;
 		if [ ! -f ${TMunb}/unbound-*/LICENSE ]; then echo -e "\n$DauCau Giải nén thất bại!!! Thoát ra!"; DonDep; exit;
 			else echo "$DauCau Giải thành công!!!"; BuildUB; fi;	
-	fi
-}
-
-KiemSH () {
-	if [ $net -ge 1 ]; then echo "$DauCau Đang kiểm tra cập nhật $(basename "$0") $PhienBan...";
-		PhienBanMoi=$(curl -sL "${UpLink}" | grep PhienBan\= | sed 's/.*\=\"//; s/\"$//');
-		if [ $PhienBanMoi == $PhienBan ]; then echo "$DauCau $(basename "$0") $PhienBan là phiên bản mới nhất!";
-			else echo "$DauCau Đang cập nhật $(basename "$0") v.$PhienBan lên v.$PhienBanMoi...";
-				cp $0 ${TMunb}/$PhienBan\_$(basename "$0");
-				curl -sLo $upTam $UpLink; chmod +x $upTam; mv $upTam $0;
-				echo "$Time $(basename "$0") được cập nhật lên $PhienBanMoi!"  >> $Log;
-				echo "$DauCau Khởi chạy $(basename "$0") $PhienBanMoi...";
-				sh ${TMunb}/$(basename "$0"); exit 1; fi
 	fi
 }
 
