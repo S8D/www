8d00ebea74ae326a2189fde7eda2407c6f4c8fec
Darias <DariasLuc@Gmail.Com>
2023-03-15 15:25:52 +0700

    Update wifi.sh

--- RT-AC58U/wifi.sh
+++ RT-AC58U/wifi.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-PhienBan="20200307c"
+PhienBan="230315a"
+echo "Đang kiểm tra gói cần thiết..."
 opkg list-installed | grep -qw curl || opkg update
 opkg list-installed | grep -qw curl || opkg install curl
-echo "Downloading WiFi driver"
-curl -sLo /root/wifi uli.vn/wifi; echo "Installing WiFi driver"
-ubiupdatevol /dev/ubi0_1 /root/wifi; echo "Press y to remove old driver"
+echo "Đang tải driver WiFi..."
+curl -sLo /root/wifi gg.gg/ac58u-wifi; echo "Đang cài driver WiFi..."
+ubiupdatevol /dev/ubi0_1 /root/wifi; echo "Nhấn phím y để gỡ driver cũ!!!"
 rm -i /lib/firmware/ath10k/pre-cal-ahb-a[08]00000.wifi.bin; 
 rmmod ath10k_pci; modprobe ath10k_pci
